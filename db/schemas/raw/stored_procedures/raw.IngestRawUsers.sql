e6766af7d01545373506d786b96849eeea9071ae
Csaba-Zsolt Pápai <csaba.papai@stud.ubbcluj.ro>
2025-05-25 18:01:12 +0300

    add IngestRawUsers stored procedure

---
+++ db/schemas/raw/stored_procedures/raw.IngestRawUsers.sql
@@ -0,0 +1,23 @@
+CREATE PROCEDURE [raw].[IngestRawUsers]
+    @UserJson NVARCHAR(MAX),
+    @Source NVARCHAR(255) = 'https://randomuser.me/api/'
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    IF OBJECT_ID('[raw].[Users]', 'U') IS NULL
+    BEGIN
+        CREATE TABLE [raw].[Users]
+        (
+            [Id] INT IDENTITY(1,1) PRIMARY KEY,
+            [Source] NVARCHAR(255) NULL DEFAULT 'https://randomuser.me/api/',
+            [UserJson] NVARCHAR(MAX) NOT NULL,
+			[InsertedAt] DATETIME DEFAULT GETDATE()
+        );
+
+    END
+
+    INSERT INTO [raw].[Users] (UserJson, Source)
+    VALUES (@UserJson, @Source);
+END
+GO
