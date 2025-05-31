831a0828dd10e781c0015344b93f59e46284694b
Csaba-Zsolt Pápai <csaba.papai@stud.ubbcluj.ro>
2025-05-31 17:03:56 +0300

    added to datalake the countris.csv and started the exchange rate workflow

--- daily_processing.py
+++ daily_processing.py
@@ -1,15 +1,18 @@
 import os
 from utils.connect_to_database import connect_to_database
 from user_processing.daily_user_processing import daily_user_processing
+from exchange_rate_processing.daily_exchange_rate_processing import daily_exchange_rate_processing
 
 
 def main():
     server, database = os.getenv('SQL_SERVER'), os.getenv('SQL_DATABASE')
     connection, cursor = connect_to_database(server, database)
 
-    # Start daily processing
-    daily_user_processing(connection, cursor)
+    # # Start daily user processing
+    # daily_user_processing(connection, cursor)
 
+    # Start daily exchange rate processing
+    daily_exchange_rate_processing(connection, cursor)
 
 if __name__ == "__main__":
     main()
